9f3f974101194c9c259aedeb03dbab574f9241ea
Owen Seltzer <seltzer.o@northeastern.edu>
2024-03-01 16:13:28 -0500

    Refactor animation code and add error handling in promise-sum-2darray

--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -3,3 +3,17 @@ const array2D = [
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+async function calculateSum() {
+    try {
+        const rowSums = await Promise.all(rowSumPromises);
+        let sum = 0;
+        rowSums.forEach((rowSum) => {
+            sum += rowSum;
+        });
+        return 'done';
+    } catch (error) {
+        console.log(error);
+        return 'failed';
+    }
+}
